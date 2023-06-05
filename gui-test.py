cdbcf5caf6000142f88fce8017ec2562f3cae819
ChromiteExabyte <135404783+ChromiteExabyte@users.noreply.github.com>
2023-06-05 15:46:11 -0400

    Create gui-test.py
    First test of tkinter window

---
+++ gui-test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Create a new Tkinter window
+window = tk.Tk()
+
+# Add a label to the window
+label = tk.Label(window, text="Hello, World!")
+label.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
